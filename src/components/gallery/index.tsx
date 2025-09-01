070d0d64e6f1ec1fd6f2fcbff7b6e796b1a1970a
Jigisha Ponda <jigishaadatiya21@gmail.com>
2025-09-01 17:24:23 +0530

    added videos component in Gallery page

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -3,6 +3,7 @@ import Breadcrumb from '../common/breadcrumb/Breadcrumb';
 import PhotoGalleryHome from '../homes/home/PhotoGalleryHome';
 import Footer from '@/layout/footers/Footer';
 import Header from '@/layout/headers/Header';
+import LatestNewsAreaHome from '../homes/home/LatestNewsAreaHome';
 
 const Gallery = () => {
   return (
@@ -11,6 +12,7 @@ const Gallery = () => {
       <main>
         <Breadcrumb sub_title='Inside MCH – Where Care Meets Excellence' title='Gallery' page='Gallery' />
         <PhotoGalleryHome/>
+        <LatestNewsAreaHome />
       </main>
       <Footer />
     </>
